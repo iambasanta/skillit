a86e8024c30d1f87c38db7c9a6b489f6fdb396f6
iambasanta <sangpangbasanta@gmail.com>
2022-07-06 13:55:58 +0545

    Show all users in admin dashboard

--- app/Http/Controllers/Admin/AdminDashobardController.php
+++ app/Http/Controllers/Admin/AdminDashobardController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminDashobardController extends Controller
 {
 
     public function index(){
-        return view('admin.index');
+        $users = User::orderBy('id')->paginate(10);
+
+        return view('admin.index',compact('users'));
     }
 
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,16 +8,13 @@
                             <thead class="bg-gray-50">
                                 <tr>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Title
+                                        Full Name
                                     </th>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Status
+                                        Email
                                     </th>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Workshops
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Sessions
+                                        Faculty
                                     </th>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Action
@@ -25,31 +22,21 @@
                                 </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach($users as $user)
                                 <tr>
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="flex items-center">
-                                            <div class="ml-4">
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    <a href="">
-                                                        Skill-IT
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                            published
+                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold ">
+                                            {{ $user->name }}
                                         </span>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <span class="px-2 inline-flex text-xs leading-5 font-semibold ">
-                                            9
+                                            {{ $user->email }}
                                         </span>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <span class="px-2 inline-flex text-xs leading-5 font-semibold ">
-                                            2
+                                            CSIT
                                         </span>
                                     </td>
                                     <td class="flex px-6 py-4 space-x-4 whitespace-nowrap text-right text-sm font-medium">
@@ -73,8 +60,13 @@
                                         </a>
                                     </td>
                                 </tr>
+                                    @endforeach
                             </tbody>
                         </table>
+                    </div>
+
+                    <div class="m-2">
+                        {{ $users->links() }}
                     </div>
                 </div>
             </div>
