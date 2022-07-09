0c0d393388164f0b57a4c63c40da89bd36ace9c4
iambasanta <sangpangbasanta@gmail.com>
2022-07-09 11:06:55 +0545

    Change background color of notification

--- resources/views/components/notify.blade.php
+++ resources/views/components/notify.blade.php
@@ -1,6 +1,6 @@
 @if(session('success'))
 
-<div x-data="{show:true}" x-init="setTimeout(()=>show=false,3000)" x-show="show" class="fixed bg-gray-800 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm">
+<div x-data="{show:true}" x-init="setTimeout(()=>show=false,3000)" x-show="show" class="fixed bg-blue-background text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm">
     <p>{{session('success')}}</p>
 </div>
 
