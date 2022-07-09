8ed7026d6d521028277f4f3bddbcf7189137acf0
iambasanta <sangpangbasanta@gmail.com>
2022-07-09 08:29:15 +0545

    Add links to navigation bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,7 @@
                     <x-nav-link :href="route('workshops')" :active="request()->routeIs('workshops')">
                         {{ __('Workshops') }}
                     </x-nav-link>
+
                     <x-nav-link :href="route('sessions')" :active="request()->routeIs('sessions')">
                         {{ __('Sessions') }}
                     </x-nav-link>
@@ -79,8 +80,12 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('workshops')" :active="request()->routeIs('workshops')">
+                {{ __('Workshops') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('sessions')" :active="request()->routeIs('sessions')">
+                {{ __('Sessions') }}
             </x-responsive-nav-link>
         </div>
 
@@ -92,6 +97,10 @@
             </div>
 
             <div class="mt-3 space-y-1">
+                <x-responsive-nav-link :href="route('user.profile')" :active="request()->routeIs('user.profile')">
+                    {{ __('Profile') }}
+                </x-responsive-nav-link>
+
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
