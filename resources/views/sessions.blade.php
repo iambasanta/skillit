ce455e1a25e12f81dbb8c9b6992de9f467670335
iambasanta <sangpangbasanta@gmail.com>
2022-07-10 16:12:36 +0545

    Add margin to increase space between elements

--- resources/views/sessions.blade.php
+++ resources/views/sessions.blade.php
@@ -54,7 +54,7 @@
                                         @endif
                                     </div>
 
-                                    <div>
+                                    <div class="text-sm">
                                         <span>Duration : {{ $session->duration }} Day</span>
                                     </div>
                                 </div>
@@ -64,7 +64,7 @@
                                     <div class="text-gray-500 ">
                                         <span>{{ $session->date }}</span>
                                         
-                                        <div class="flex items-center bg-gray-200 px-2 py-1 rounded-full lg:max-w-full max-w-fit">
+                                        <div class="flex items-center bg-gray-200 px-2 py-1 rounded-full lg:max-w-full max-w-fit mt-4">
                                             <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
                                             </svg>
